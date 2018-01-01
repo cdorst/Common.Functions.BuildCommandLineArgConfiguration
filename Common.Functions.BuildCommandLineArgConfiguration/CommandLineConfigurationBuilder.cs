695f90f01a13a928d07df73172c5d9f14faa081a
Christopher Dorst <chris@dorst.email>
2018-01-01 01:40:55 -0600

    Add CommandLineConfigurationBuilder

---
+++ Common.Functions.BuildCommandLineArgConfiguration/CommandLineConfigurationBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Common.Functions.BuildCommandLineArgConfiguration
+{
+    public static class CommandLineConfigurationBuilder
+    {
+        public static IConfigurationRoot Configuration(string[] args)
+            => new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+    }
+}
